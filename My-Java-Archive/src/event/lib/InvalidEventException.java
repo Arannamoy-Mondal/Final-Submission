a7e9bdb5bee6eb3d367e2cf21f6e156ddc93f512
Arannamoy Mondal <71834368+Arannamoy-Mondal@users.noreply.github.com>
2024-12-12 13:06:10 +0600

    Delete My-Java-Archive directory

--- My-Java-Archive/src/event/lib/InvalidEventException.java
+++
@@ -1,9 +0,0 @@
-package event.lib;
-
-public class InvalidEventException
-        extends Exception {
- public InvalidEventException(String id) {
-           super(String.format("Event with id:{%s} is not a valid Event.", new Object[] { id }));
-         }
-    }
-
